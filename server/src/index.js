a03e95c6bc5112c1745b8b269ad7a00d2f482472
Aakash Kumar <95979529+aakashvani@users.noreply.github.com>
2023-09-16 14:29:57 +0000

    changes

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 
-// app.use(express.json());
+app.use(express.json());
 
 const urlRoute = require("./routes/urlRoute");
 
-app.use("api/url", urlRoute);
+app.use("/api", urlRoute);
 module.exports = app;
