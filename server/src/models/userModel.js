f5654166a4d170ee37ff53e86745ab484726cef7
Aakash Kumar <95979529+aakashvani@users.noreply.github.com>
2023-09-23 19:44:52 +0530

    Install npm packages for user auth and created schema.

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -1,21 +1,26 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const validator = require("validator");
 
 const userSchema = new Schema(
   {
     fullName: {
       type: String,
-      require: true,
+      require: [true, "Please enter your full name"],
+      minLength: [1, "Name must be at least 2-3 characters"],
     },
     email: {
       type: String,
-      require: true,
+      require: [true, "Please enter your email address"],
       unique: true,
+      validate: [validator.isEmail, "Please enter a valid email"],
     },
     password: {
       type: String,
-      require: true,
-      maxLength: 8,
+      require: [true, "Please enter a strong password"],
+      validate: validator.isPassword,
+      minLength: [8, "Password must be at least 8 characters"],
+      select: false,
     },
   },
   { timestamps: true }
