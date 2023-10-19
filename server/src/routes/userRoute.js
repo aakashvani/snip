8ae9c3bdcc2befa4cd53b5e56055ad2a0094cef3
Aakash Kumar <95979529+aakashvani@users.noreply.github.com>
2023-10-19 16:32:08 +0530

    seperated client from backend

--- server/src/routes/userRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { handleSignUp } = require("../controllers/userController");
-
-router.route("/signup").post(handleSignUp);
-
-module.exports = router;
